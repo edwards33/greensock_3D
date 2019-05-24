6fb2b87b8a817c0bc6fdd2f2711c145ab5222572
Edward <edylennyy@gmail.com>
2019-05-24 23:03:38 +0300

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+## GreenSock JS 3d animation of div
+
+  Install [GreenSock](https://greensock.com/gsap)/all dependences:
+
+```bash
+$ npm i
+```
+
+  Run Animation:
+
+```bash
+$ npm run dev
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+import { TweenMax } from "gsap"
+
+const box = document.createElement("div")
+box.setAttribute("class", "box")
+document.body.appendChild(box)
+
+box.addEventListener("click", () => {
+  TweenMax.to(box, 1, { rotationY: "+=360" })
+})
